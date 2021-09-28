3ba63f19f627fbe888a072d9eebb87d3da77a7de
Jhonssegura <jhonathanssegura@gmail.com>
2021-09-28 12:01:24 -0500

    Agregando mas funcionalidades

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 // Supports ES6
 // import { create, Whatsapp } from 'venom-bot';
 const venom = require('venom-bot');
+const fs = require('fs');
 
 venom
-  .create()
-  .then((client) => start(client))
+  .create(
+    'sessionName',
+    (base64Qr, asciiQR, attempts, urlCode) => {
+      // console.log(asciiQR); // Optional to log the QR in the terminal
+      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+      if (matches.length !== 3) {
+        return new Error('Invalid input string');
+      }
+      response.type = matches[1];
+      response.data = new Buffer.from(matches[2], 'base64');
+
+      var imageBuffer = response;
+      require('fs').writeFile(
+        'images/qt-code/qt.png',
+        imageBuffer['data'],
+        'binary',
+        function (err) {
+          if (err != null) {
+            console.log(err);
+          }
+        }
+      );
+    },
+    undefined,
+    { logQR: false }
+  )
+  .then((client) => { 
+    start(client);
+  })
   .catch((erro) => {
     console.log(erro);
   });
@@ -18,7 +48,6 @@ function start(client) {
     .then((result) => {
       console.log('Result: ', result); //return object success
     })
-
     
     .catch((erro) => {
       console.error('Error when sending: ', erro); //return object error
@@ -56,6 +85,22 @@ function start(client) {
         .catch((erro) => {
           console.error('Error when sending: ', erro); //return object error
         });
+    }
+    if (message.body === 'Imagen' || message.body === 'imagen') {
+      // Send and image
+      await client
+      .sendImage(
+        message.from,
+        'images/response.jpg',
+        'image-name',
+        'Caption text'
+      )
+      .then((result) => {
+        console.log('Result: ', result); //return object success
+      })
+      .catch((erro) => {
+        console.error('Error when sending: ', erro); //return object error
+      });
     }
   });
 }
